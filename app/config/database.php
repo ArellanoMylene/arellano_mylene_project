17c1feed0e1f7470b830f6ebfbcc608bb6b4047b
ARELLANO_MYLENE <153874323+ArellanoMylene@users.noreply.github.com>
2025-09-29 15:03:56 +0800

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -70,15 +70,16 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
     // 'path'      => ''
 
     
-$database['main'] = array(
-    'driver'    => 'mysql',
-    'hostname'  => getenv("DB_HOST"),
-    'port'      => getenv("DB_PORT"),
-    'username'  => getenv("DB_USER"),
-    'password'  => getenv("DB_PASS"),
-    'database'  => getenv("DB_NAME"),
-    'charset'   => 'utf8mb4',
-    'dbprefix'  => ''
+    'driver'	=> 'mysql',
+    'hostname'	=> 'sql12.freesqldatabase.com',
+    'port'		=> '3306',
+    'username'	=> 'sql12800395',
+    'password'	=> 'C4jtkGbRUj',
+    'database'	=> 'sql12800395',
+    'charset'	=> 'utf8',
+    'dbprefix'	=> '',
+    // Optional for SQLite
+    'path'      => ''
 );
 
 
